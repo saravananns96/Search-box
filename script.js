d081837b4561a444a841929fe211203e7ded258b
unknown <saravanankgm96@gmail.com>
2021-09-17 23:00:13 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const search=document.querySelector('.search');
+const button=document.querySelector('.btn');
+
+const input=document.querySelector('.input');
+
+button.addEventListener('click',()=>{
+	search.classList.toggle('active');
+	input.focus();
+})
