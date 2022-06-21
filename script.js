27b9869d174dd3702d77c8521416ec689eafd3c8
unknown <saravanankgm96@gmail.com>
2022-06-21 14:18:15 +0530

    add new stylesheet

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-'use strict'
+"use strict";
 
-const search=document.querySelector('.search');
-const button=document.querySelector('.btn');
+const search = document.querySelector(".search");
+const button = document.querySelector(".btn");
+console.log("msg");
+const input = document.querySelector(".input");
 
-const input=document.querySelector('.input');
-
-button.addEventListener('click',()=>{
-	search.classList.toggle('active');
-	input.focus();
-})
+button.addEventListener("click", () => {
+  search.classList.toggle("active");
+  input.focus();
+});
